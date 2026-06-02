a91aabeb4cd288fffff0893301334b527b264a64
poweroffinace <rittwick.finance@gmail.com>
2026-06-02 19:40:27 +0000

    added the std dev too

--- filter_stock.py
+++ filter_stock.py
@@ -14,14 +14,18 @@ for item in data:
     
     row = [symbol, marketCap]
     for day in range(MAX_DAYS+1):
-        row.append(item.get('data').get(f'day_{day}').get('mean'))
+        mean = item.get('data').get(f'day_{day}').get('mean')
+        std  = item.get('data').get(f'day_{day}').get('std')
+        row.append(mean)
+        row.append(mean-std)
     df.append(row)
 
 columns = ['symbol', 'marketCap']
 for day in range(MAX_DAYS+1):
     columns.append(f'day_{day}')
+    columns.append(f'day_{day}_min')
 df = pd.DataFrame(df, columns=columns)
 
-df.sort_values(by=['day_0', 'day_1', 'day_2', 'day_3', 'day_4'], ascending=False, inplace=True)
+df.sort_values(by=['day_0', 'day_1', 'day_2', 'day_3', 'day_4', 'day_5'], ascending=False, inplace=True)
 
 df.to_csv('displacement_df.csv', index=False)
